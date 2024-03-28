98a958122448d9b3a4a51f8244f4e39ba25af10e
hodong <hodong42@gmail.com>
2024-03-28 16:47:42 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+  token = var.ggtk
+}
+# Add a user to the organization
+
+
+resource "github_repository" "example" {
+  name        = "example"
+  description = "My awesome codebase"
+
+  visibility = "public"
+}
+
+variable "ggtk" {
+  type = string
+}
+
+output "repository_url" {
+  value = github_repository.example.http_clone_url
+}
